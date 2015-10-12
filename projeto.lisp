641dde0482d254c355b7de218c8e60669f3f5c43
Diogo Painho <painho.diogo@gmail.com>
2015-10-12 23:36:27 +0100

    Pus todas as funcoes precisas e a implementar

--- projeto.lisp
+++ projeto.lisp
@@ -7,26 +7,78 @@
 ;;;;;;;;;;;;;;;;;;;;
 ;;;; TIPO ACCAO ;;;;
 ;;;;;;;;;;;;;;;;;;;;
+(defstruct (accao (:constructor cria-accao (inteiro array)))
+  inteiro
+  array)
 
+(defun accao-coluna (array)
+  accao-inteiro array)
+
+(defun accao-peca (accao))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; TIPO TABULEIRO ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun cria-tabuleiro ())
+
+(defun copia-tabuleiro (tabuleiro))
+
+(defun tabuleiro-preenchido-p (tabuleiro inteiro1 inteiro2))
+
+(defun tabuleiro-altura-coluna (tabuleiro inteiro))
+
+(defun tabuleiro-linha-completa-p (tabuleiro inteiro))
+
+(defun tabuleiro-preenche! (tabuleiro inteiro1 inteiro2))
+
+(defun tabuleiro-remove-linha! (tabuleiro inteiro))
+
+(defun tabuleiro-topo-preenchido-p (tabuleiro))
+
+(defun tabuleiros-iguais-p (tabuleiro1 tabuleiro2))
+
+(defun tabuleiro->array (tabuleiro))
+
+(defun array->tabuleiro (array))
 
 
 ;;;;;;;;;;;;;;;;;;;;;
 ;;;; TIPO ESTADO ;;;;
 ;;;;;;;;;;;;;;;;;;;;;
 
+(defun copia-estado (estado))
+
+(defun estados-iguais-p (estado1 estado2))
+
+(defun estado-final-p (estado))
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; TIPO PROBLEMA ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;
+(defstruct problema estado-inicial solucao accoes resultado custo-caminho)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; FUNCOES DO PROBLEMA DE PROCURA ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun solucao (estado))
+
+(defun accoes (estado))
+
+(defun resultado (estado accao))
+
+(defun qualidade (estado))
+
+(defun custo-oportunidade (estado))
+
+
 ;;;;;;;;;;;;;;;;;
 ;;;; PROCURA ;;;;
 ;;;;;;;;;;;;;;;;;
+
+(defun procura-pp (problema))
+
+(defun procura-A* (problema heuristica))
+
+(defun procura-best (array lista-pecas)) 
